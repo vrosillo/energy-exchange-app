365387b376b3e7ae06f164e1339c67d1b518dc79
vrosillo <63453485+vrosillo@users.noreply.github.com>
2020-06-28 11:17:53 +0200

    Showing on the web exchange data

--- src/App.js
+++ src/App.js
@@ -22,7 +22,8 @@ export class App extends Component {
         super(props);
         this.state = {
           account: undefined,
-          balance :0 
+          balance :0,
+          sellOffers: []
         };
     }  
 
@@ -33,37 +34,77 @@ export class App extends Component {
         //defines an instance of the contact exchange
         this.exchange = await ExchangeContract(this.web3.currentProvider);
         this.exchangeService = new ExchangeService(this.exchange);
+        
         //utilities
         this.toEther = converter(this.web3);
 
         var account = (await this.web3.eth.getAccounts())[0];
         
         this.setState({
-          account: account.toLowerCase()
+          account: account
         },()=>{
           this.load();
         });
     }
 
-    async addOffer(){
-      await this.exchangeService.newSell(this.state.account,10,10);
-    }
+    /////////////////////
+    //Account functions//
+    /////////////////////
 
     async getBalance(){
       let weiBalance = await this.web3.eth.getBalance(this.state.account);
       this.setState({
         balance:this.toEther(weiBalance)
       });
-    
     }
+    ///////////////////
+    //Agent functions//
+    ///////////////////
+
+    async getAgents() {
+      await this.exchangeService.getAgentContracts();
+
+    }
+
+  
+
+    async newAgent(){
+      await this.exchangeService.createAgentContract(this.state.account);
+    }
+
+    //////////////////
+    //Sell functions//
+    //////////////////
+
+    async addOffer(){
+      await this.exchangeService.newSell(this.state.account,10,10,this.state.account);
+    }
+
+    /*
+    async getOffer(){
+      await this.exchangeService.getSellOffers();
+    }
+    */
+
+    async getOffer() {
+      let sellOffers = await this.exchangeService.getSellOffers();
+      this.setState({
+        sellOffers
+      });
+  }
+
+    ////////
+    //load--execute a function at the beggining of everything//
+    ////////
+    
     async load(){
       this.getBalance();
-      this.addOffer();
+      this.getOffer();
+      
     }
 
     render() {
-        return (
-          <React.Fragment>
+      return <React.Fragment>
           <div className="jumbotron">
               <h4 className="display-4">Welcome to the Exchange!</h4>
           </div>
@@ -71,19 +112,55 @@ export class App extends Component {
           <div className="row">
               <div className="col-sm">
                   <Panel title="Balance">
-                      <p><strong>Account: </strong>{this.state.account}</p>
-                      <span><strong>Balance (ether): </strong>{this.state.balance}</span>
+                        <p><strong>{this.state.account}</strong></p>
+                        <span><strong>Balance</strong>: {this.state.balance}</span>
                   </Panel>
               </div>
               <div className="col-sm">
-                  <Panel title="Agent Id">
-                      <button onClick={()=> this.addOffer()}>AddSellOffer</button>
-                      <p>´{this.state.agentDetails}</p>
-
+                  <Panel title="2">
+                      
+                  </Panel>
+              </div>
+          </div>
+          <div className="row">
+              <div className="col-sm">
+                  <Panel title="Agents">
+                    <span>Deployed agents</span>
+                    <button onClick={()=> this.getAgents()}>GetAgentDetails</button>
+                    <button onClick={()=> this.newAgent()}>AddNewAgent</button>   
+                                
+                  </Panel>
+              </div>
+              <div className="col-sm">
+                  <Panel title="Sells">
+                      <button onClick={()=> this.getOffer()}>GetSellOffer</button>
+                      <button onClick={()=> this.addOffer()}>SaveOffer</button>   
+                  </Panel>
+              </div>
+          </div>
+          <div className="row">
+              <div className="col-sm">
+                  <Panel title="Buy">
+                      <span>Buy orders</span>
+                      <button>Add new buy</button>
+                  </Panel>
+              </div>
+              <div className="col-sm">
+                  <Panel title="List of sells">
+                  {this.state.sellOffers.map((offer, i) => {
+                            return <div key={i}>
+                                <span>Id:{offer.OrderId}  </span>
+                                <span>Agent:{offer.OrderAgent}  </span>
+                                <span>Unit:{offer.OrderUnit}</span>
+                                <span>Price:{offer.OrderPricePerUnit}  </span>
+                                <span>TotalPrice:{offer.OrderTotalPrice}  </span>
+                                <span>IsAvailable:{offer.OrderIsAvailable}  </span>
+                               
+                            </div>
+                        })}
                   </Panel>
               </div>
           </div>
       </React.Fragment>
-          );
-    }
+  }
 }
--- src/ContractsInteraction/exchangeService.js
+++ src/ContractsInteraction/exchangeService.js
@@ -8,8 +8,58 @@ export class ExchangeService{
     //Sell functions//
     //////////////////
 
-    async newSell(_agentAddr,_pricePerUnit,_unit){
-        return this.contract.saveSellOrder(_agentAddr,_pricePerUnit,_unit);
+    async newSell(_agentAddr,_pricePerUnit,_unit,from){
+        return this.contract.saveSellOrder(_agentAddr,_pricePerUnit,_unit,{from});
     }
+
+    async getSellOffers(){
+        
+        //get sell offers
+        let sellOffers = await this.contract.getSellOffers();
+        const {0:arrSellOrderId,1:arrSellOrderAgent,2:arrSellOrderUnit,
+            3:arrSellOrderPricePerUnit,4:arrSellOrderTotalPrice,5:arrSellOrderIsAvailable}=sellOffers;
+
+        //get total number of sell offers  
+        let total = arrSellOrderId.length;
+
+        //descompose sell offers into objects
+        let offers = [];
+        for (var i = 0; i < total; i++) {
+            let offer = {
+                OrderId:arrSellOrderId[i].toNumber(),
+                OrderAgent:arrSellOrderAgent[i],
+                OrderUnit:arrSellOrderUnit[i].toNumber(),
+                OrderPricePerUnit:arrSellOrderPricePerUnit[i].toNumber(),
+                OrderTotalPrice:arrSellOrderTotalPrice[i].toNumber(),
+                OrderIsAvailable:arrSellOrderIsAvailable[i].toString()
+            }
+            
+            offers.push(offer);
+        }
+
+        //log
+        console.log(sellOffers);
+        console.log(offers);
+
+        return offers;
+    }
+
+ 
+   
+    ///////////////////
+    //Agent functions//
+    ///////////////////
+    async getAgentContracts(){
+        let agentDetails = await this.contract.getDeployedContractAgentDetails();
+
+        const {0:agentId,1:agentAddress,2:agentContractAddress,3:agentIsMember}=agentDetails;
+        console.log(agentId.toNumber(),agentAddress,agentContractAddress,agentIsMember);
+        
+    }
+
+    createAgentContract(from){
+         this.contract.createAgentContract(1,1,10,{from});
+    }
+
 
 }
